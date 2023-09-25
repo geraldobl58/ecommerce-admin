9b1974967873103a8e78876ac7f6a3083817e483
Geraldo Luiz <geraldobl58@gmail.com>
2023-09-25 19:30:29 -0300

    feat: Creating components and category forms

---
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -0,0 +1,209 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { useParams, useRouter } from "next/navigation";
+
+import * as z from "zod";
+
+import { Billboard, Category } from "@prisma/client";
+
+import toast from "react-hot-toast";
+
+import axios from "axios";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Trash } from "lucide-react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modal/alert-modal";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+interface CategoryFormProps {
+  initialData: Category | null;
+  billboards: Billboard[];
+}
+
+const formSchema = z.object({
+  name: z.string().min(3, {
+    message: "Campo obrigátorio",
+  }),
+  billboardId: z.string().min(1, {
+    message: "Campo obrigátorio",
+  }),
+});
+
+type CategoryFormValues = z.infer<typeof formSchema>;
+
+export const CategoryForm = ({
+  initialData,
+  billboards,
+}: CategoryFormProps) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const params = useParams();
+  const router = useRouter();
+
+  const title = initialData ? "Editar categoria" : "Nova categoria";
+  const description = initialData ? "Editar categoria" : "Adicionar categoria";
+  const toastMessage = initialData
+    ? "Categoria autualizada com sucesso!"
+    : "Categoria adicionada com sucesso!";
+  const action = initialData ? "Editar" : "Salvar";
+
+  const form = useForm<CategoryFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      billboardId: "",
+    },
+  });
+
+  const onSubmit = async (data: CategoryFormValues) => {
+    try {
+      setLoading(true);
+
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/categories/${params.categoryId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/categories`, data);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/categories`);
+      toast.success(toastMessage);
+    } catch (error) {
+      console.log(error);
+      toast.error("Houve um erro ao atualizar!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(
+        `/api/stores/${params.storeId}/categories/${params.categoryId}`
+      );
+      router.refresh();
+      router.push(`/${params.storeId}/categories`);
+      toast.success("Registro excluido com sucesso!");
+    } catch (error) {
+      toast.error("Certifique-se de remover todos os produtos.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="icon"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Titulo</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Digite o titulo"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="billboardId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Banner</FormLabel>
+                  <Select
+                    disabled={loading}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue
+                          defaultValue={field.value}
+                          placeholder="Selecione o banner"
+                        />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {billboards.map((item) => (
+                        <SelectItem key={item.id} value={item.id}>
+                          {item.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" disabled={loading} className="ml-auto">
+            {action}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+import { CategoryColumn, columns } from "./columns";
+
+interface CategoryClientProps {
+  data: CategoryColumn[];
+}
+
+export const CategoryClient = ({ data }: CategoryClientProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categorias (${data.length})`}
+          description="Gerencie suas categorias."
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Novo Registro
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="Chamada de API para categorias" />
+      <Separator />
+      <ApiList entityName="categories" entityIdName="categoryId" />
+    </>
+  );
+};
